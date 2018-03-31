d20be2f64025b87897c45cb2caba003f2a862f7e
Guanghui Wang <guanghui-wang@foxmail.com>
2018-03-31 23:12:09 +0800

    Add another service unit testing, different from vote service way

--- src/app/services/event.service.spec.ts
+++ src/app/services/event.service.spec.ts
@@ -1,14 +1,58 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+  TestRequest,
+} from '@angular/common/http/testing';
+
 import { EventService } from './event.service';
 
+import { IEvent, CommonError } from '../models';
+
 describe('EventService', () => {
+  let eventService: EventService;
+  let httpTestingController: HttpTestingController;
+
+  const events: IEvent[] = [
+    {
+      id: 1,
+      name: 'Angular Connect',
+      date: new Date('9/26/2036'),
+      time: '10:00 am',
+      price: 599.99,
+      imageUrl: '/assets/images/angularconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England',
+      },
+      sessions: [],
+    },
+    {
+      id: 2,
+      name: 'ng-nl',
+      date: new Date('4/15/2037'),
+      time: '9:00 am',
+      price: 950.0,
+      imageUrl: '/assets/images/ng-nl.png',
+      onlineUrl: 'http://github.com',
+      sessions: [],
+    },
+  ];
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [EventService],
     });
+
+    eventService = TestBed.get(EventService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it(
@@ -17,4 +61,39 @@ describe('EventService', () => {
       expect(service).toBeTruthy();
     }),
   );
+
+  it('should GET all events', () => {
+    eventService.getEvents().subscribe((data: IEvent[]) => {
+      expect(data.length).toBe(2);
+    });
+
+    const eventsRequest: TestRequest = httpTestingController.expectOne('/api/events');
+    expect(eventsRequest.request.method).toEqual('GET');
+
+    eventsRequest.flush(events);
+  });
+
+  it('should return a CommonError', () => {
+    eventService.getEvents().subscribe(
+      (data: IEvent[]) => fail('this should have been an error'),
+      (err: any) => {
+        console.error(err);
+        expect(err.error.code).toEqual(500);
+        expect(err.error.message).toEqual('An error occurred retrieving data.');
+      },
+    );
+
+    const eventsRequest: TestRequest = httpTestingController.expectOne('/api/events');
+
+    eventsRequest.flush(
+      {
+        code: 500,
+        message: 'An error occurred retrieving data.',
+      },
+      {
+        status: 500,
+        statusText: 'Server Error',
+      },
+    );
+  });
 });
